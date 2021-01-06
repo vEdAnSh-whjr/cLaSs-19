a1f81c10ebdcdcc6e6570cb5c335b74d7fbf0832
vEdAnSh-whjr <76542652+vEdAnSh-whjr@users.noreply.github.com>
2021-01-06 19:51:43 +1100

    collision detection algorithm

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# cLaSs-19
-collision detection
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,33 @@
+var fixedRect, movingRect
+function setup() {
+  createCanvas(800,400);
+  fixedRect = createSprite(400, 200, 50, 50);
+  movingRect = createSprite (600, 300, 50, 70)
+  fixedRect.shapeColor = "green" 
+  movingRect.shapeColor = "green"
+
+  fixedRect.debug = true
+  movingRect.debug = true
+ 
+}
+
+function draw() {
+  background(0); 
+  movingRect.y = mouseY
+  movingRect.x = mouseX
+
+  if (movingRect.x - fixedRect.x < movingRect.width/2+fixedRect.width/2 &&
+    fixedRect.x - movingRect.x < movingRect.width/2+fixedRect.width/2 &&
+    movingRect.y - fixedRect.y < movingRect.height/2+fixedRect.height/2 &&
+    fixedRect.y - movingRect.y < movingRect.height/2+fixedRect.height/2 )  {
+  fixedRect.shapeColor = "red"
+  movingRect.shapeColor = "red"
+  }
+  else{
+  fixedRect.shapeColor = "green" 
+  movingRect.shapeColor = "green"
+  }
+  drawSprites();
+  
+
+}
